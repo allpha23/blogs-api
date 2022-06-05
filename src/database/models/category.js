25c4954a49e6903e932f968a0cdfef2ed4b40d5f
allpha23 <allpha23@gmail.com>
2022-06-04 23:52:17 -0300

    add model e migration de category

---
+++ src/database/models/category.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return Category;
+};
