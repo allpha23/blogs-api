bdd45ce96c56cf5fe61fa655bb84bfc8ffa56e9f
allpha23 <allpha23@gmail.com>
2022-06-04 23:51:44 -0300

    add model e migration de user

---
+++ src/database/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return User;
+};
