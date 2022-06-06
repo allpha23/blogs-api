fef9ce87681d7c4ed0602108200f526ca7c43ebd
allpha23 <allpha23@gmail.com>
2022-06-06 14:29:36 -0300

    fix: (loginController) dataValues undefined

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -7,13 +7,12 @@ router.post('/', validation, async (req, res) => {
   const { email, password } = req.body;
     
   const findUser = await loginService.getAll(email, password);
-
-  const { id } = findUser[0].dataValues;
     
   if (findUser.length === 0) {
     return res.status(400).json({ message: 'Invalid fields' });
   }
-    
+
+  const { id } = findUser[0].dataValues;   
   const token = jwtGenerator({ id, email });
     
   return res.status(200).json({ token });
