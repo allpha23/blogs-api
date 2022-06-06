64b9718715e9283f0de5e1aed2d4237d414405a4
allpha23 <allpha23@gmail.com>
2022-06-06 13:47:52 -0300

    fix: (login/token) recebendo corretamente userId

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -7,12 +7,14 @@ router.post('/', validation, async (req, res) => {
   const { email, password } = req.body;
     
   const findUser = await loginService.getAll(email, password);
+
+  const { id } = findUser[0].dataValues;
     
   if (findUser.length === 0) {
     return res.status(400).json({ message: 'Invalid fields' });
   }
     
-  const token = jwtGenerator({ id: findUser.id, email });
+  const token = jwtGenerator({ id, email });
     
   return res.status(200).json({ token });
 });
